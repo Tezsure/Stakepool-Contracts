91847d60499e63f14da159d5e38f2fa8fa122c58
Aditya Gautam <60175624+aditya-tezsure@users.noreply.github.com>
2021-04-21 14:10:01 +0530

    modifying code for netlify support

--- stakepool-cron/Mainnet/handler.js
+++ stakepool-cron/Mainnet/handler.js
@@ -1,47 +1,43 @@
 require('dotenv').config();
 
-const Tezos = require('@taquito/taquito');
-const InMemorySigner = require('@taquito/signer');
-const rpc = 'https://mainnet-tezos.giganode.io';
-const tezos = new Tezos.TezosToolkit(rpc);
+const { TezosToolkit } = require('@taquito/taquito');
+const { InMemorySigner } = require('@taquito/signer');
+const {
+    MAINNET_SECRET_KEY,
+    MAINNET_CONTRACT_ADDRESS,
+    MAINNET_HARBINGER_CONTRACT_ADDRESS,
+    MAINNET_RPC_URL,
+    TIMER,
+} = process.env;
 
 const pingContractMainnet = async () => {
-    console.log('----- Ping contract start -----');
-    const {
-        MAINNET_SECRET_KEY,
-        MAINNET_CONTRACT_ADDRESS,
-        MAINNET_HARBINGER_CONTRACT_ADDRESS,
-    } = process.env;
-    const signer = await InMemorySigner.InMemorySigner.fromSecretKey(
-        MAINNET_SECRET_KEY
-    );
-    tezos.setProvider({ signer });
-
-    tezos.contract
-        .at(MAINNET_CONTRACT_ADDRESS)
-        .then((contract) => {
-            return contract.methods
-                .fetchPriceAndUpdateCycle(
-                    'XTZ-USD',
-                    MAINNET_HARBINGER_CONTRACT_ADDRESS,
-                    MAINNET_CONTRACT_ADDRESS
-                )
-                .send();
-        })
-        .then((op) => {
-            console.log('------------- Operation -----------------');
-            console.log(op);
-            console.log(op.hash);
-            return op.confirmation();
-        })
-        .then((hash) => {
-            console.log('------------- Hash -----------------');
-            console.log(hash);
-        })
-        .catch((err) => {
-            console.log('------------- Error -----------------');
-            console.log(err);
-        });
+    try {
+        console.log('----- Ping contract start -----', MAINNET_SECRET_KEY);
+        const rpc = TESTNET_RPC_URL;
+        const Tezos = new TezosToolkit(rpc);
+        const signer = new InMemorySigner(MAINNET_SECRET_KEY);
+        // eslint-disable-next-line no-restricted-properties
+        Tezos.setProvider({ signer });
+        const contract = await Tezos.contract.at(MAINNET_CONTRACT_ADDRESS);
+        const operation = await contract.methods
+            .fetchPriceAndUpdateCycle(
+                'XTZ-USD',
+                MAINNET_HARBINGER_CONTRACT_ADDRESS,
+                MAINNET_CONTRACT_ADDRESS
+            )
+            .send();
+        console.log('----------- operation --------', operation);
+        await operation.confirmation(1).then(() => operation.opHash);
+        setTimeout(() => {
+            pingContractTestnet();
+        }, TIMER);
+    } catch (error) {
+        console.log('----------- Error --------', error);
+        console.error(error);
+        setTimeout(() => {
+            pingContractTestnet();
+        }, TIMER);
+    }
 };
 
-exports.pingContractMainnet = pingContractMainnet;
+pingContractMainnet();
--- stakepool-cron/Testnet/handler.js
+++ stakepool-cron/Testnet/handler.js
@@ -1,52 +1,43 @@
-'use strict';
 require('dotenv').config();
 
-const Tezos = require('@taquito/taquito');
-const InMemorySigner = require('@taquito/signer');
-const rpc = process.env.TESTNET_RPC_URL;
-const tezos = new Tezos.TezosToolkit(rpc);
+const { TezosToolkit } = require('@taquito/taquito');
+const { InMemorySigner } = require('@taquito/signer');
+const {
+    TESTNET_SECRET_KEY,
+    TESTNET_CONTRACT_ADDRESS,
+    TESTNET_HARBINGER_CONTRACT_ADDRESS,
+    TESTNET_RPC_URL,
+    TIMER,
+} = process.env;
 
 const pingContractTestnet = async () => {
-    console.log('----- Ping contract start -----');
-    const {
-        TESTNET_SECRET_KEY,
-        TESTNET_CONTRACT_ADDRESS,
-        TESTNET_HARBINGER_CONTRACT_ADDRESS,
-    } = process.env;
-
-    const signer = await InMemorySigner.InMemorySigner.fromSecretKey(
-        TESTNET_SECRET_KEY
-    );
-    tezos.setProvider({
-        signer,
-    });
-
-    tezos.contract
-        .at(TESTNET_CONTRACT_ADDRESS)
-        .then((contract) => {
-            return contract.methods
-                .fetchPriceAndUpdateCycle(
-                    'XTZ-USD',
-                    TESTNET_HARBINGER_CONTRACT_ADDRESS,
-                    TESTNET_CONTRACT_ADDRESS
-                )
-                .send();
-        })
-        .then((op) => {
-            console.log('------------- Operation -----------------');
-            console.log(op);
-            return op.confirmation();
-        })
-        .then((hash) => {
-            console.log('------------- Hash -----------------');
-            console.log(hash);
+    try {
+        console.log('----- Ping contract start -----', TESTNET_SECRET_KEY);
+        const rpc = TESTNET_RPC_URL;
+        const Tezos = new TezosToolkit(rpc);
+        const signer = new InMemorySigner(TESTNET_SECRET_KEY);
+        // eslint-disable-next-line no-restricted-properties
+        Tezos.setProvider({ signer });
+        const contract = await Tezos.contract.at(TESTNET_CONTRACT_ADDRESS);
+        const operation = await contract.methods
+            .fetchPriceAndUpdateCycle(
+                'XTZ-USD',
+                TESTNET_HARBINGER_CONTRACT_ADDRESS,
+                TESTNET_CONTRACT_ADDRESS
+            )
+            .send();
+        console.log('----------- operation --------', operation);
+        await operation.confirmation(1).then(() => operation.opHash);
+        setTimeout(() => {
             pingContractTestnet();
-        })
-        .catch((err) => {
-            console.log('------------- Error -----------------');
-            console.log(err.message);
+        }, TIMER);
+    } catch (error) {
+        console.log('----------- Error --------', error);
+        console.error(error);
+        setTimeout(() => {
             pingContractTestnet();
-        });
+        }, TIMER);
+    }
 };
 
-exports.pingContractTestnet = pingContractTestnet;
+pingContractTestnet();
